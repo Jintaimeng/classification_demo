66d56497457a4f229af6172f678ee96950977a72
Jintaimeng <929357244@qq.com>
2021-07-05 22:27:06 +0800

    im_data

--- readcifar10.py
+++ readcifar10.py
@@ -1,3 +1,5 @@
+import pickle
+import numpy as np
 def unpickle(file):
     import pickle
     with open(file, 'rb') as fo:
@@ -13,3 +15,15 @@ label_name = ["airplane",
               "horse",
               "ship",
               "truck"]
+import glob
+
+train_list = glob.glob("/home/jinyue/PycharmProjects/cifar-10-python/data_batch_*")
+print(train_list)
+for l in train_list:
+    print(l)
+    l_dict = unpickle(l)
+    #print(l_dict)
+    print(l_dict.keys())
+    for im_idx, im_data in enumerate(l_dict[b'data']):
+        print(im_idx)
+        print(im_data)
